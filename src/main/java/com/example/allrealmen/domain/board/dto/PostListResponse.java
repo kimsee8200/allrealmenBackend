5678e95331ea7249fdd6b0b3e7a47fb5c4fae944
kimsee <ttungseui11@gmail.com>
2025-06-25 23:43:04 +0900

    allreal backend 배포

---
+++ src/main/java/com/example/allrealmen/domain/board/dto/PostListResponse.java
@@ -0,0 +1,32 @@
+package com.example.allrealmen.domain.board.dto;
+
+import com.example.allrealmen.domain.board.entity.Post;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class PostListResponse {
+    private String id;
+    private String title;
+    private String writer;
+    private int watch;
+    private int comment;
+    
+    @JsonProperty("create_at")
+    private LocalDateTime createAt;
+
+    public static PostListResponse from(Post post) {
+        return PostListResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .writer(post.getWriterId())
+                .watch(post.getWatch())
+                .comment(post.getComment())
+                .createAt(post.getCreateAt())
+                .build();
+    }
+} 
---
+++ src/main/java/com/example/allrealmen/domain/user/dto/MemberUpdateDto.java
@@ -0,0 +1,11 @@
+package com.example.allrealmen.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberUpdateDto {
+    private String name;
+    private String phoneNumber;
+} 
