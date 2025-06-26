1623b2de56dc9807005fbae43c5059d49905ffc0
kimsee <ttungseui11@gmail.com>
2025-06-26 15:51:34 +0900

    allreal backend 체팅 도메인 업데이트 및 버그 수정

--- src/main/java/com/example/allrealmen/config/SecurityConfig.java
+++ src/main/java/com/example/allrealmen/config/SecurityConfig.java
@@ -148,7 +148,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 
                 // 나머지 요청
-                .anyRequest().denyAll()
+                .anyRequest().authenticated()
             )
             // JWT 필터 추가 (Form 로그인 필터 이전에 추가)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
