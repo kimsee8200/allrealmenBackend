44fe7800d70805de5fe236ffd5c637f94d326eb8
kimsee8200 <ttungseui11@gmail.com>
2025-07-02 08:39:46 +0900

    allreal backend 체팅 도메인 업데이트 및 버그 수정

--- src/main/java/com/example/allrealmen/config/SecurityConfig.java
+++ src/main/java/com/example/allrealmen/config/SecurityConfig.java
@@ -87,9 +87,8 @@ public class SecurityConfig {
             "http://localhost:3000",
             "http://localhost:5173",
                 "https://www.xn--q20b38omkn.com/",
-                "http://allrealwebserver:81",
-                "43.201.168.53"
-
+                "https://www.xn--q20b38omkn.com",
+                "http://allrealwebserver:81"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
